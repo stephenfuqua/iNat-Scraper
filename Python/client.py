4a57572b8b92a213194f12def8230fa1eba624e4
Stephen Fuqua <stephen@safnet.com>
2021-01-01 20:09:22 -0600

    Use last_id style paging

--- Python/client.py
+++ Python/client.py
@@ -57,8 +57,8 @@ def _evaluate_response(response: Response):
     max_calls_total=REQUEST_RETRY_COUNT,
     retry_window_after_first_call_in_seconds=REQUEST_RETRY_TIMEOUT_SECONDS,
 )
-def _get_page(config: Configuration, page_number: int, headers: dict) -> dict:
-    url = f"https://api.inaturalist.org/v1/observations?pcid=true&project_id={config.project_slug}&per_page={config.page_size}&order=desc&order_by=created_at&page={page_number}"
+def _get_page(config: Configuration, last_id: str, headers: dict) -> dict:
+    url = f"https://api.inaturalist.org/v1/observations?pcid=true&project_id={config.project_slug}&per_page={config.page_size}&order_by=id&order=asc&id_above={last_id}"
 
     r = get(url, headers)
     _evaluate_response(r)
@@ -69,36 +69,10 @@ def _get_page(config: Configuration, page_number: int, headers: dict) -> dict:
 
     return r.json()
 
-# TODO: redo paging based on this description, since some projects have too many
-# records
-"""
-The page and per_page parameters can be used to fetch up to (for many endpoints)
-10k results. An error will be thrown if results beyond 10k are requested. This
-is due to database memory implications of paging through large result sets
-
-Any change the search parameters, like adding date filters, or filtering by
-taxon, will be a new search. So try changing filters instead of paging through a
-huge result set
-
-Be sure to use enough search filters so that you’re only fetching the data you
-plan to use
-
-If more than 10k results are needed, that would be considered a bulk request,
-which are not best handled by the API. See the “Bulk Requests” section below
-
-One way to use the API and fetch more than 10k records is to sort by id
-ascending (e.g. &order_by=id&order=asc) and use the id_above parameter set to
-the ID of the record in the last batch. id_above is a search parameter, so this
-is essentially the same recommendation as above to change search parameters, but
-by using a parameter that doesn’t affect the nature of the results
-"""
 def _there_are_more_results(response: dict):
     total = response["total_results"]
     page = response["page"]
     per_page = response["per_page"]
-
-    # Temporary
-    #return page * per_page > 5
 
     return page * per_page < total
 
@@ -110,24 +84,24 @@ def get_project_data(config: Configuration) -> List[dict]:
     Parameters
     ----------
     config: Configuration
-        A custom Configuration object containing important settings
+        A custom Configuration object containing important settings.
 
     Returns
     -------
-    A list of observations, each of which is a dictionary
+    A list of observations, each of which is a JSON-like dictionary.
     """
 
     results = list()
 
     headers = _build_header(config)
 
-    page_number = 1
-    response = _get_page(config, page_number, headers)
+    response = _get_page(config, 0, headers)
     results += response["results"]
 
     while _there_are_more_results(response):
-        page_number += 1
-        response = _get_page(config, page_number, headers)
+        last_id = response["results"][-1]["id"]
+
+        response = _get_page(config, last_id, headers)
         results += response["results"]
 
     return results
