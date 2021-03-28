96cee41cf6fda8f328ae4c15202edf7acb319357
Stephen A. Fuqua <stephen@safnet.com>
2021-03-28 14:45:35 -0500

    Fix bug with lat/long

--- inat_project_extractor/__main__.py
+++ inat_project_extractor/__main__.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from pprint import pprint as print
 import sys
@@ -6,7 +7,7 @@ from dotenv import load_dotenv
 from errorhandler import ErrorHandler  # type: ignore
 
 from client import get_project_data
-from configuration import get_configuration
+from configuration import get_configuration, Configuration
 from export import export, build_file_path
 
 def _configure_logging(log_level: str):
@@ -24,17 +25,12 @@ def _configure_logging(log_level: str):
 
     return logger, error_tracker
 
-def main():
-    load_dotenv()
-    config = get_configuration(sys.argv[1:])
+def _test_with_json_file(file_path: str):
+    out = json.load(open("C:\\source\\iNat-Scraper\\inat_project_extractor\\t-out\\five-observations.json",))
+    export(file_path, out["results"])
 
-    logger, error_tracker = _configure_logging(config.log_level)
 
-    logger.info("Starting iNaturalist project data extractor.")
-    logger.info(f"Configuration: {config}")
-
-    file_path = build_file_path(config)
-
+def _run(config: Configuration, file_path: str):
     while 1==1:
         project_data = get_project_data(config)
 
@@ -45,6 +41,19 @@ def main():
         export(file_path, project_data)
 
         config.last_id = project_data[-1]["id"]
+
+
+def main():
+    load_dotenv()
+    config = get_configuration(sys.argv[1:])
+
+    logger, error_tracker = _configure_logging(config.log_level)
+
+    logger.info("Starting iNaturalist project data extractor.")
+    logger.info(f"Configuration: {config}")
+
+    file_path = build_file_path(config)
+    _run(config, file_path)
 
     logger.info("Finished with data extraction")
 
